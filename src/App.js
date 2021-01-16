edf8705077215bfc76aa677d8ac6986952c6d21e
vishnukakkarayil <vishnuprakash.khd@gmail.com>
2021-01-16 20:05:37 +0530

    Initial Upload

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import Home from './components/home/Home'
 function App() {
-useEffect(() =>{ axios('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=y0pGKNASy3k8cP39dmvdwjPGaPG8dLhk')
-.then(data => console.log(data))
+useEffect(() =>{ axios.get('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=y0pGKNASy3k8cP39dmvdwjPGaPG8dLhk')
+.then(data => console.log(data.data))
 .catch(err=>{ alert(err) })
 },[])
   return (
